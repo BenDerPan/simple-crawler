b45cfaf1c92f2ac5fda4eb83336432c1faecd563
A.T.M <1295213184@qq.com>
2016-05-11 14:19:29 +0800

    完成了正大广场的活动的爬虫,标题加链接.

--- routes/api.js
+++ routes/api.js
@@ -9,25 +9,28 @@ var router = express.Router();
 
 
 router.get('/', function (req, res, next) {
-    console.log('start');
-    next();
-}, function (req, res, next) {
     //在这里我们进行爬虫
+
+    var baseUrl = 'http://www.superbrandmall.com/sbm/';
     superagent.get('http://www.superbrandmall.com/sbm/share?mode=e')
         .end(function (err, sres) {
             // 常规的错误处理
             if (err) {
                 return next(err);
-            }            // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-            // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-            // 剩下就都是 jquery 的内容了
+            }
+
             var $ = cheerio.load(sres.text);
-            var items = [];            [].forEach.call($('.share-box').find('h1'), function (index, item) {
-                items.push(index.children[0].data);
+            var items = [];
+
+            //遍历所有的div快对其进行过滤得到标题和链接
+            [].forEach.call($('.share-box'), function (element, index) {
+                items.push({
+                    "title": $(element).find('h1')[0].children[0].data,
+                    "url": baseUrl + $(element).find('p.bttn-share-view-all').find('a').attr('href')
+                })
             });
             res.send(items);
         });
-
 });
 
 module.exports = router;
